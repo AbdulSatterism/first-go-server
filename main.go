a145240fd1775e91d92083aa588ea327c60fae22
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-11-10 01:01:38 +0600

    implement get routes in go lang and get all products

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 func introduceMe(w http.ResponseWriter, r *http.Request) {
@@ -13,33 +13,86 @@ func introduceMe(w http.ResponseWriter, r *http.Request) {
 func aboutMe(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "about me")
 }
-func dynamicRoute(w http.ResponseWriter, r *http.Request) {
 
-	id := strings.Split(r.URL.Path, "/")[2]
+// get all products
 
-	fmt.Fprintln(w, "dynamic route: ", id)
+// product struct
+type IProduct struct {
+	ID          int `json:"id"` // it converted into small leter id
+	Name        string
+	Price       float64
+	Stock       int
+	Description string
 }
 
-func homeRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "welcome to home to my server")
+var productList []IProduct
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("content-type", "application/json")
+
+	if r.Method != "GET" {
+		http.Error(w, "This method not allow", 400)
+		return
+	}
+
+	encode := json.NewEncoder(w)
+
+	encode.Encode(productList)
+
 }
 
 func main() {
 
 	mux := http.NewServeMux() // this is route
 
-	mux.HandleFunc("/", homeRoute)
 	mux.HandleFunc("/about", introduceMe)
 
 	mux.HandleFunc("/home", aboutMe)
-	mux.HandleFunc("/home/{id}", dynamicRoute)
+
+	// adding new route for products
+
+	mux.HandleFunc("/products", getProducts)
 
 	fmt.Println("server running on port :3000")
 
 	err := http.ListenAndServe(":3000", mux)
 
 	if err != nil {
-		fmt.Println("something went wrong server is not runnign", err)
+		fmt.Println("something went wrong server is not running", err)
 	}
+
+}
+
+func init() {
+
+	// some demo product
+
+	product1 := IProduct{
+		ID:          1,
+		Name:        "Laptop",
+		Price:       45000.00,
+		Stock:       5,
+		Description: "This is a gaming laptop",
+	}
+
+	product2 := IProduct{
+		ID:          2,
+		Name:        "Mobile Phone",
+		Price:       25000.00,
+		Stock:       10,
+		Description: "This is a smartphone",
+	}
+
+	product3 := IProduct{
+		ID:          3,
+		Name:        "Headphones",
+		Price:       5000.00,
+		Stock:       15,
+		Description: "This is a wireless headphone",
+	}
+
+	productList = append(productList, product1, product2, product3)
 
 }
